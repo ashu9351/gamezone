1e1d3a4589544efdaf15b95addc25cd4a8c2485d
ashu9351 <ashutosh.bhardwaj9@gmail.com>
2021-07-20 11:12:38 +0530

    nvigationdone

--- screens/home.js
+++ screens/home.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {StyleSheet , Text , View } from 'react-native'
+import {StyleSheet , Text , View ,Button} from 'react-native'
 import { globalStyles } from '../styles/global'
-export default function Home(){
+export default function Home( {navigation} ){
+    const pressHandler = ()=>{
+        navigation.navigate('ReviewDetail');
+    }
     return(
         <View style={globalStyles.container}>
             <Text style={globalStyles.header}> Home Page </Text>
+            <Button title='Go To Review Details' onPress={pressHandler}/>
         </View>
     );
 }
--- screens/reviewdetail.js
+++ screens/reviewdetail.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {StyleSheet , Text , View } from 'react-native'
+import {StyleSheet , Text , View ,Button} from 'react-native'
 import { globalStyles } from '../styles/global'
 
-export default function ReviewDetail(){
+export default function ReviewDetail( {navigation}){
+    const pressHandler = ()=>{
+        navigation.goBack();
+    }
     return(
         <View style={globalStyles.container}>
             <Text style={globalStyles.header}> Review detail Page </Text>
+            <Button title='Go To Home' onPress={pressHandler}/>
         </View>
     );
 }
